59de5c86d061b0ca450fa10ebd57d725c4f57231
Ross Novick <rossnovick@pacbell.net>
2016-02-25 19:02:55 -0800

    add dependencies

---
+++ components/classifieds.ctr.js
@@ -0,0 +1,16 @@
+(function() {
+
+	"use strict";
+
+	angular
+		.module("ngClassifieds")
+		.controller("classifiedsCtrl", function($scope) {
+
+			$scope.name = {
+				first: "Ross",
+				last: "Novick"
+			};
+
+		});
+
+})();
